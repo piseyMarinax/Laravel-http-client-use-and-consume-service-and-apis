ea113a17c1af909b72c6259ff640c5ea23ba5367
pisey <pisey@marinax.biz>
2018-05-02 23:01:44 +0700

    Creating a Layout a View and a Route

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>HTTP Client</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js" integrity="sha384-u/bQvRA/1bobcXlcEYpsEdFVK/vJs3+T+nXLsBYJthmdBuavHvAW6UsmqO2Gd/F9" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="container-fluid">
+        @yield('content')
+    </div>
+</body>
+</html>
---
+++ resources/views/main.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('content')
+<h2>HTTP Client</h2>
+<ui class="list-group">
+     <li class="list-group-item"><a href=" ">Show all students</a></li>
+     <li class="list-group-item"><a href=" ">Show all teachers</a></li>
+     <li class="list-group-item"><a href=" ">Show all course</a></li>
+</ui>
+
+@endsection
