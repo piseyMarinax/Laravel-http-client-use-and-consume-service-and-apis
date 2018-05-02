d1f029d4cdec8cbd241d23a922538eb8d01966bd
pisey <pisey@marinax.biz>
2018-05-02 22:32:11 +0700

    Configuring Laravel and The Project

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Exceptions;
+namespace HttpClient\Exceptions;
 
 use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
--- config/app.php
+++ config/app.php
@@ -154,11 +154,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        HttpClient\Providers\AppServiceProvider::class,
+        HttpClient\Providers\AuthServiceProvider::class,
+        // HttpClient\Providers\BroadcastServiceProvider::class,
+        HttpClient\Providers\EventServiceProvider::class,
+        HttpClient\Providers\RouteServiceProvider::class,
 
     ],
 
