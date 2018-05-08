ba571caee790543e2bdf2a68b37b178b2dd1a815
pisey <pisey@marinax.biz>
2018-05-08 14:03:20 +0700

    - Create ClientController nad cacert.pem

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace HttpClient\Http\Controllers;
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+use HttpClient\Http\Requests;
+
+class ClientController extends Controller
+{
+    protected function performRequest($method,$url,$parameter=[])
+    {
+        $client = new Client(['curl' => [CURLOPT_CAINFO => base_path('resources/certs/cacert/pem')]]);
+        $response = $client->request($method, $url, $parameter);
+
+        return $response->getBody()->getContents();
+    }
+}
